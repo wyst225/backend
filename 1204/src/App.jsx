26be8d88041dfaf213de214d58187d9a12a164ea
wyst225 <abelvilmos99@gmail.com>
2025-12-04 11:09:06 +0100

    1204.1

---
+++ 1204/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1>Egyszerű számláló</h1>
+      <div id="kezeloPanel">
+        <button>+</button>
+        <p>&nbsp;</p>
+        <button>-</button>
+      </div>
+    </>
+  )
+}
+
+export default App
