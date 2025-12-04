befed118b18ac54f40201b7a2146a55848ab7469
wyst225 <abelvilmos99@gmail.com>
2025-12-04 12:22:33 +0100

    .2

--- 1204/src/App.jsx
+++ 1204/src/App.jsx
@@ -1,14 +1,32 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(8);
+
+  useEffect(() => {
+    const upButton = document.querySelector('.up');
+    const downButton = document.querySelector('.down');
+
+    const increment = () => setCount(prevCount => prevCount + 1);
+    const decrement = () => setCount(prevCount => prevCount - 1);
+
+    upButton.addEventListener('click', increment);
+    downButton.addEventListener('click', decrement);
+
+    return () => {
+      upButton.removeEventListener('click', increment);
+      downButton.removeEventListener('click', decrement);
+    };
+  }, []);
 
   return (
     <>
       <h1>Egyszerű számláló</h1>
       <div id="kezeloPanel">
-        <button>+</button>
-        <p>&nbsp;</p>
-        <button>-</button>
+        <button className='up'>+</button>
+        <p>{count}</p>
+        <button className='down'>-</button>
       </div>
     </>
   )
